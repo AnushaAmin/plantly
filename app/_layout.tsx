6fe9d825fb4f3b51762e687b5baa18df8aae9027
Anusha <anusha.amin03@gmail.com>
2025-04-15 02:15:21 +0500

    aaddd

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,19 @@
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+import * as QuickActions from "expo-quick-actions";
 
 
 export default function Layout () {
+    useEffect(() => {
+        QuickActions.setItems([
+            {
+           title:"Add a plant",
+           icon:"symbol:leaf",
+           id:"0"
+        }
+    ])
+        
+    }, [])
     return(
         <Stack>
             <Stack.Screen name="(tabs)" options={{headerShown:false, animation:"fade"}}/>
